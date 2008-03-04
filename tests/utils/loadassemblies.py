f283e3f884103e4bbaf99d04180a50c67e15171d
fwereade <fwereade@localhost>
2008-03-04 10:42:47 +0000

    * Removed unnecessary PythonStubHarness files
    by: William

--- tests/utils/loadassemblies.py
+++ tests/utils/loadassemblies.py
@@ -1,6 +1,5 @@
 
 import clr
 clr.AddReferenceToFile("build/ironclad.dll")
-clr.AddReferenceToFile("tests/data/ironcladtestutils.dll")
 clr.AddReference("IronPython.dll")
 clr.AddReference("IronMath.dll")
