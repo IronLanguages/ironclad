b957b3064345dcb4e5dbc20f36434c9fdd1e9b42
fwereade <fwereade@localhost>
2008-08-30 23:17:31 +0000

    * Attempts to get numpy tests running have revealed that __displayhook__ also seems to occasionally disappear from sys. Put in with the executable reset; at least that specific ugliness is localised ;).
    by: William

--- tests/bugtest.py
+++ tests/bugtest.py
@@ -4,7 +4,7 @@ from tests.utils.testcase import TestCase
 
 class BugTest(TestCase):
     def testDisplayhook(self):
-        self.assertEquals(hasattr(sys, '__displayhook__'), False, "build/ironclad.py no longer needs to set sys.__displayhook__ = sys.displayhook")
+        self.assertEquals(hasattr(sys, '__displayhook__'), False, "Python25Mapper.MessWithSys may no longer need to set sys.__displayhook__ = sys.displayhook")
 
 suite = makesuite(BugTest)
 if __name__ == '__main__':
