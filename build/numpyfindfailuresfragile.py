a785b512de44b7f58e7a71af5665c32c0ef8697c
fwereade <fwereade@localhost>
2008-12-04 18:41:10 +0000

    * Fixed numpyfindfailuresfragile * Added several more tests to blacklist
    by: William

--- build/numpyfindfailuresfragile.py
+++ build/numpyfindfailuresfragile.py
@@ -4,7 +4,7 @@ import sys
 from System.Diagnostics import Process
 from numpytests import get_continuation_point, add_to_blacklist, save_continuation_point
 
-verbose =  '--verbose' in sys.argv or '-v' in sys.argv:
+verbose =  '--verbose' in sys.argv or '-v' in sys.argv
 
 def run_blacklister_robustly():
     p = Process()
@@ -14,17 +14,12 @@ def run_blacklister_robustly():
     p.StartInfo.UseShellExecute = not verbose
     p.Start()
     if not p.WaitForExit(120000):
-	add_to_blacklist(get_continuation_point(), msg='Appeared to block')
-	save_continuation_point(None)
-	p.Kill()
-	p.WaitForExit()
-	return False
+        add_to_blacklist(get_continuation_point(), msg='Appeared to block')
+        save_continuation_point(None)
+        p.Kill()
+        p.WaitForExit()
+        return False
     return p.ExitCode == 0
 
 while not run_blacklister_robustly():
     pass
- 
-    
-    
-
-
