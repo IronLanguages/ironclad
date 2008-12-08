3268086f730bcfddd9e73678b5a7358c2c4154ee
fwereade <fwereade@localhost>
2008-12-08 17:32:16 +0000

    * Unblacklisted several tests; apart from docstring problems, umath and ufunc tests all pass * LastException now always stores the appropriate Python exception if possible * new InappropriateReflection method, for getting Python exceptions * PyErr_Occurred now matches spec * Copied CPython implementations of PyErr_ExceptionMatches, PyObject_CallFunctionObjArgs, PyObject_CallMethodObjArgs * Tweaked PyErr_GivenExceptionMatches
    by: Orestis, William

--- tests/utils/testcase.py
+++ tests/utils/testcase.py
@@ -31,10 +31,7 @@ class TestCase(unittest.TestCase):
         
     def assertMapperHasError(self, mapper, error):
         if error:
-            self.assertNotEquals(mapper.LastException, None, "no error set on mapper")
-            def KindaConvertError():
-                raise mapper.LastException
-            self.assertRaises(error, KindaConvertError)
+            self.assertEquals(isinstance(mapper.LastException, error), True, "wrong error set on mapper")
             mapper.LastException = None
         else:
             self.assertEquals(mapper.LastException, None)
