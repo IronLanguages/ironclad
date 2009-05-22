e40340cb1dd88a67bf97867d03b6fec7697683ec
fwereade <fwereade@localhost>
2009-05-22 01:10:39 +0000

    * 'implement' some PyObject_Gc_ functions (just call the non-GC version) * rearrange GC-related IRONCLAD macros to make more sense * of 74 tests run by test.test_csv.test_main: 8 errors, 1 failure * w00t!
    by: William

--- stub/Include/objimpl.h
+++ stub/Include/objimpl.h
@@ -261,6 +261,9 @@ extern PyGC_Head *_PyGC_generation0;
 
 /* Tell the GC to track this object.  NB: While the object is tracked the
  * collector it must be safe to call the ob_traverse method. */
+#ifdef IRONCLAD // don't do anything
+#define _PyObject_GC_TRACK(o)
+#else //IRONCLAD
 #define _PyObject_GC_TRACK(o) do { \
 	PyGC_Head *g = _Py_AS_GC(o); \
 	if (g->gc.gc_refs != _PyGC_REFS_UNTRACKED) \
@@ -271,11 +274,15 @@ extern PyGC_Head *_PyGC_generation0;
 	g->gc.gc_prev->gc.gc_next = g; \
 	_PyGC_generation0->gc.gc_prev = g; \
     } while (0);
+#endif //IRONCLAD
 
 /* Tell the GC to stop tracking this object.
  * gc_next doesn't need to be set to NULL, but doing so is a good
  * way to provoke memory errors if calling code is confused.
  */
+#ifdef IRONCLAD // don't do anything
+#define _PyObject_GC_UNTRACK(o)
+#else //IRONCLAD
 #define _PyObject_GC_UNTRACK(o) do { \
 	PyGC_Head *g = _Py_AS_GC(o); \
 	assert(g->gc.gc_refs != _PyGC_REFS_UNTRACKED); \
@@ -284,6 +291,7 @@ extern PyGC_Head *_PyGC_generation0;
 	g->gc.gc_next->gc.gc_prev = g->gc.gc_prev; \
 	g->gc.gc_next = NULL; \
     } while (0);
+#endif // IRONCLAD
 
 PyAPI_FUNC(PyObject *) _PyObject_GC_Malloc(size_t);
 PyAPI_FUNC(PyObject *) _PyObject_GC_New(PyTypeObject *);
--- stub/ironclad-hacks.c
+++ stub/ironclad-hacks.c
@@ -13,6 +13,11 @@ PyEval_GetRestricted(void)
 }
 
 
+PyObject * 
+_PyObject_GC_Malloc(size_t basicsize)
+{
+    return PyObject_Malloc(basicsize);
+}
 
 PyObject *
 _PyObject_GC_New(PyTypeObject *tp)
@@ -26,4 +31,14 @@ _PyObject_GC_NewVar(PyTypeObject *tp, Py_ssize_t nitems)
 	return _PyObject_NewVar(tp, nitems);
 }
 
+void
+PyObject_GC_Del(void *op)
+{
+	PyObject_Del(op);
+}
+
+
+
+void PyObject_GC_Track(void *_) {}
+void PyObject_GC_UnTrack(void *_) {}
 
