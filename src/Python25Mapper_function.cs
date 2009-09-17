0332ab972d8c9b44c313f5b3c5c3bbabb11c7454
fwereade <fwereade@localhost>
2009-09-17 15:46:00 +0000

    * blacklist tweaks * PythonFunctions are now stored as PyFunctionObjects filled with nulls -- this is good enough for scipy.fftpack to run, because it always uses API getattrs for stuff like .func_code.co_argcount. It isn't good enough to make it return the right results, but hey.
    by: William

---
+++ src/Python25Mapper_function.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.InteropServices;
+
+using IronPython.Runtime;
+
+using Ironclad.Structs;
+
+namespace Ironclad
+{
+    public partial class Python25Mapper : Python25Api
+    {
+        private IntPtr
+        StoreTyped(PythonFunction func)
+        {
+            uint size = (uint)Marshal.SizeOf(typeof(PyFunctionObject));
+            IntPtr ptr = this.allocator.Alloc(size);
+            CPyMarshal.Zero(ptr, size);
+            CPyMarshal.WriteIntField(ptr, typeof(PyIntObject), "ob_refcnt", 1);
+            CPyMarshal.WritePtrField(ptr, typeof(PyIntObject), "ob_type", this.PyFunction_Type);
+            this.map.Associate(ptr, func);
+            return ptr;
+        }
+    }
+}
