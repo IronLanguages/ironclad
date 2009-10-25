d4d091894db6d28e860331f55451f04c28cd6360
fwereade <fwereade@localhost>
2009-10-25 00:24:31 +0000

    * tools/generate*.py now badge their outputs * tools.utils.write now chooses badge format by file extension
    by: William

--- tools/generatestub.py
+++ tools/generatestub.py
@@ -8,7 +8,7 @@ src_dll, src, dst = sys.argv[1:]
 dst_include = os.path.join(dst, 'Include')
 
 sm = StubMaker(src_dll, src)
-write(dst, "stubinit.generated.c", sm.generate_c())
-write(dst, "jumps.generated.asm", sm.generate_asm())
-write(dst_include, "_mgd_function_prototypes.generated.h", sm.generate_header())
+write(dst, "stubinit.generated.c", sm.generate_c(), badge=True)
+write(dst, "jumps.generated.asm", sm.generate_asm(), badge=True)
+write(dst_include, "_mgd_function_prototypes.generated.h", sm.generate_header(), badge=True)
 
