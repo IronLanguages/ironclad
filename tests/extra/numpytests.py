9ddedcd2e7d6155358b97210ee4bdc2d34f0c35c
fwereade <fwereade@localhost>
2009-10-28 14:19:55 +0000

    * ipy -X:Frames tests\extra\numpytests.py now runs 1399 tests without errors
    by: William

--- tests/extra/numpytests.py
+++ tests/extra/numpytests.py
@@ -1,22 +1,16 @@
 
 np_path = r'C:\Program Files\IronPython 2.6\lib\site-packages\numpy'
 np_blacklist = r'tests\extra\numpy_test_blacklist'
+np_excludes = r'distutils f2py'.split()
 
-
-import sys
+import re, sys
 sys.path.append('build')
 
 import ironclad
 ironclad.patch_native_filenos()
 
+from tests.utils.blacklists import BlacklistConfig
+config = BlacklistConfig(np_blacklist, np_excludes)
+
 import nose
-config = nose.config.Config()
-config.verbosity = 3
-
-import re
-config.exclude = (re.compile('distutils'), re.compile('f2py'))
-
-from tests.utils.blacklists import BlacklistPlugins
-config.plugins = BlacklistPlugins(np_blacklist)
-
 nose.run(defaultTest=np_path, config=config)
--- tests/utils/blacklists.py
+++ tests/utils/blacklists.py
@@ -1,7 +1,10 @@
 
 from tools.utils import read_interesting_lines
 
+import re
+
 import nose
+from nose.config import Config
 from nose.selector import Selector
 from nose.plugins import Plugin
 from nose.plugins.manager import DefaultPluginManager
@@ -20,13 +23,21 @@ def if_wanted(f):
 
 class BlacklistSelector(Selector):
 
-    def __init__(self, config, blacklist):
+    def __init__(self, config, test_blacklist, file_blacklist):
         Selector.__init__(self, config)
-        self.blacklist = blacklist
+        self.test_blacklist = test_blacklist
+        self.file_blacklist = file_blacklist
         self.mod = None
+        
+    @if_wanted
+    def wantFile(self, f):
+        for f_end in self.file_blacklist:
+            if f.endswith(f_end):
+                return False
+        return True
 
     def _want(self, *args):
-        return args not in self.blacklist
+        return args not in self.test_blacklist
         
     @if_wanted
     def wantModule(self, m):
@@ -47,20 +58,37 @@ class BlacklistSelector(Selector):
         return self._want(self.mod, cls, meth)
 
 
+def _read_blacklist(f):
+    tests, files = set(), set()
+    for line in read_interesting_lines(f):
+        if line.endswith('.py'):
+            files.add(line)
+        else:
+            tests.add(tuple(line.split('.')))
+    return tests, files
+
 
 def BlacklistPlugins(blacklist_file):
-    blacklist_contents = read_interesting_lines(blacklist_file)
-    blacklist = set(map(tuple, [l.split('.') for l in blacklist_contents]))
-    
+    tests, files = _read_blacklist(blacklist_file)
     class BlacklistPlugin(Plugin):
         enabled = True
         def configure(self, _, __):
             pass
         def prepareTestLoader(self, loader):
-            loader.selector = BlacklistSelector(loader.config, blacklist)
+            loader.selector = BlacklistSelector(loader.config, tests, files)
     
     class BlacklistPluginManager(DefaultPluginManager):
         def loadPlugins(self):
             self.addPlugin(BlacklistPlugin())
 
     return BlacklistPluginManager()
+
+
+def BlacklistConfig(blacklist_file, excludes=()):
+    config = Config()
+    config.verbosity = 3
+    config.plugins = BlacklistPlugins(blacklist_file)
+    if excludes: config.exclude = map(re.compile, excludes)
+    return config
+
+
