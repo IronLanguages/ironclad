1070c3db416c78c18e3c1e47fd668734b345ea91
fwereade <fwereade@localhost>
2009-11-02 16:38:25 +0000

    * further scipy test tweaks
    by: William

--- tests/extra/scipytests.py
+++ tests/extra/scipytests.py
@@ -1,5 +1,5 @@
 
-sp_path = r'C:\Program Files\IronPython 2.6\lib\site-packages\scipy\sparse\tests\test_base.py'
+sp_path = r'C:\Program Files\IronPython 2.6\lib\site-packages\scipy'
 sp_blacklist = r'tests\extra\scipy_test_blacklist'
 
 import re, sys
@@ -15,12 +15,21 @@ import nose
 nose.run(defaultTest=sp_path, config=config)
 
 
+# cluster: 152 tests ok
+# fftpack: 25 tests ok
+# integrate: 16 tests ok
 # interpolate: cannot import factorial
-# io: cannot import zlib
+# io: 33 tests, 1 error (import gzip)
 # lib: 176 tests, 39 errors
 # linalg: 402 tests, 51 errors
-# misc: needs PIL
+# maxentropy: 2 tests ok
+# misc: needs PIL, which doesn't work in ipy ATM
+# ndimage: 414 tests ok
+# odr: 4 tests ok
+# optimize: 29 tests ok
 # signal: cannot import factorial
 # sparse: 418 tests, 19 errors
-# stats: (wedged after many passes and some errors)
-# weave: cannot import zlib
+# spatial: 229 tests ok (some pretty slow though)
+# special: 368 tests ok
+# stats: 215 tests ok
+# weave: needs signal module
